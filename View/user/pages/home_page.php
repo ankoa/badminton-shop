5668849dcc9c9e80db38430097b14ba04861a62f
ankoa <akhoa0805@gmail.com>
2024-03-17 15:33:58 +0700

    update

--- View/user/pages/home_page.php
+++ View/user/pages/home_page.php
@@ -83,7 +83,97 @@
         </div>
 
     </div>
+    <!--
     <div class="slide_product">
+        <section class="slider_product">
+                <div class="slider_product-content-top-container">
+                    <div class="slider_product-content-top">
+                    <?php
+               
+                /*require_once __DIR__ . '/../../../Model/ModelRacket.php';
+                require_once __DIR__ . '/../../../Model/ModelShutte.php';
+                require_once __DIR__ . '/../../../Model/ModelString.php';
 
-    </div>
+                $modelRacket = new ModelRacket();
+                $modelShuttle = new ModelShuttle();
+                $modelString = new ModelString();
+
+                $rackets = $modelRacket->getAllRackets();
+                $shuttles = $modelShuttle->getAllShuttles();
+                $strings = $modelString->getAllStrings();
+
+                $latestItems = [];
+
+                function findLatestItem($items) {
+                    $latestItem = null;
+                    $latestDate = null;
+                    foreach ($items as $item) {
+                        if ($latestDate === null || $item->getDateUpdated() > $latestDate) {
+                            $latestItem = $item;
+                            $latestDate = $item->getDateUpdated();
+                        }
+                    }
+                    return $latestItem;
+                }
+
+                $latestItems['racket'] = findLatestItem($rackets);
+                $latestItems['shuttle'] = findLatestItem($shuttles);
+                $latestItems['string'] = findLatestItem($strings);
+
+                foreach ($latestItems as $item) {
+                    if ($item !== null) {
+                        echo '<div class="slide-item">';
+                        echo '<img src="' . $item->getListImage() . '" alt="' . $item->getName() . '">';
+                        echo '<h3>' . $item->getName() . '</h3>';
+                        echo '<p>' . $item->getPrice() . '</p>';
+                        echo '</div>';
+                    }
+                }*/
+                ?>
+                    </div>
+                    <div class="slider-content-top-next">
+                    <i class="fa-solid fa-left-long"> </i>
+                    <i class="fa-solid fa-right-long"> </i>
+                    </div>
+         </div>
+                
+    </section>
+    </div>-->
+</div>
+<section class="section_3_banner">
+	<div class="container_3_banner">
+		<div class="title_modules">
+			<h2> 
+				<a href="" title="Thanh lý">
+					<span>Sale off</span>
+                </a>
+			</h2>
+		</div>
+		<div class="row-banners">
+            <div class="column-banner">
+                <div class="banner-item">
+                    <a href="/thanh-ly/vot-cau-long.html" title="Vợt Cầu Lông">
+                        <img width="400" height="150" src="https://cdn.shopvnb.com/uploads/danh_muc/3_2.webp" alt="Vợt Cầu Lông">
+                    </a>
+                </div>
+            </div>
+            <div class="column-banner">
+                <div class="banner-item">
+                    <a href="/thanh-ly/giay-cau-long.html" title="Giày Cầu Lông">
+                        <img width="400" height="150" src="https://cdn.shopvnb.com/uploads/danh_muc/2.webp" alt="Giày Cầu Lông">
+                    </a>
+                </div>
+            </div>
+            <div class="column-banner">
+                <div class="banner-item">
+                    <a href="/thanh-ly/ao-cau-long.html" title="Áo Cầu Lông">
+                        <img width="400" height="150" src="https://cdn.shopvnb.com/uploads/danh_muc/1_3.webp" alt="Áo Cầu Lông">
+                    </a>
+                </div>
+            </div>
+        </div>
+
+
+	</div>
+</section>
 </div>
