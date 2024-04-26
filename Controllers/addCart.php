3378e4e8fbab7d723c5807aea8cc14ff8cdf7a44
Dikay3105 <huhuhuhuvbd@gmail.com>
2024-04-27 00:19:12 +0700

    update change quantity cart product

--- Controllers/addCart.php
+++ Controllers/addCart.php
@@ -5,39 +5,43 @@ require_once(__DIR__ . '/../Model/ModelProduct.php');
 require_once(__DIR__ . '/../Model/ModelCatalog.php');
 require_once(__DIR__ . '/../Model/Entity/Catalog.php');
 
-$modeluser= new ModelUser();
-$modelcartdetail= new ModelCartDetail();
-$modelproduct= new ModelProduct();
+$modeluser = new ModelUser();
+$modelcartdetail = new ModelCartDetail();
+$modelproduct = new ModelProduct();
 $modelCatalog = new ModelCatalog();
 
 //$_SESSION['login']
 
 if (isset($_GET['productID']) && isset($_GET['username']) && isset($_GET['variantID']) && isset($_GET['quantity']) && isset($_GET['price'])) {
     // Lấy dữ liệu từ yêu cầu AJAX
-    if($modelcartdetail->checkCartDetail($modeluser->getUIDByUserName($_GET['username']), $_GET['productID'], $_GET['variantID'])==false) {
+    if ($modelcartdetail->checkCartDetail($modeluser->getUIDByUserName($_GET['username']), $_GET['productID'], $_GET['variantID']) == false) {
         $productID = $_GET['productID'];
         $variantID = $_GET['variantID'];
         $quantity = $_GET['quantity'];
         $price = $_GET['price'];
         $username = $_GET['username'];
-        $userID= $modeluser->getUIDByUserName($username);
-        $modelcartdetail->addCartDetail($userID, $productID, $variantID, $quantity,$price);
+        $userID = $modeluser->getUIDByUserName($username);
+        $modelcartdetail->addCartDetail($userID, $productID, $variantID, $quantity, $price);
         echo "k trung";
     } else {
-        $product=$modelproduct->getProductByID($_GET['productID']);
-        $quantity=$_GET['quantity']+$modelcartdetail->getQuantityCartDetail($_GET['productID'], $_GET['variantID']);
-        $price= $product->getPrice()*$quantity;
+        $product = $modelproduct->getProductByID($_GET['productID']);
+        $quantity = $_GET['quantity'] + $modelcartdetail->getQuantityCartDetail($_GET['productID'], $_GET['variantID']);
+        $price = $product->getPrice() * $quantity;
         $modelcartdetail->updateAddCartDetail($modeluser->getUIDByUserName($_GET['username']), $_GET['productID'], $_GET['variantID'], $quantity, $price);
         echo 'trung';
     }
     $catalog = $modelCatalog->getCatalogByID($_GET['productID']);
-    
-    
-} else if (isset($_GET['productID']) && isset($_GET['cartID']) && isset($_GET['variantID'])) {
-    $modelcartdetail->deleteProductCartDetail($_GET['cartID'], $_GET['productID'], $_GET['variantID']);
+}else if(isset($_GET['action'])) {
+    if($_GET['action']=="delete") {
+        $modelcartdetail->deleteProductCartDetail($_GET['cartID'], $_GET['productID'], $_GET['variantID']);
+    } elseif($_GET['action']=="quantity") {
+        if($_GET['quantity']>0) {
+            $product=$modelproduct->getProductByID($_GET['productID']);
+            $modelcartdetail->updateQuantityCartDetail($_GET['cartID'], $_GET['productID'], $_GET['variantID'], $_GET['quantity'], $product->getPrice()*$_GET['quantity']);
+        }
+    } 
 } else {
     echo json_encode([]);
 }
 
 
-?>
