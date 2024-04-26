84078dd2ee9a214eea88e29e7429ee6cba23cb8c
Khoa910 <khoa9102003@gmail.com>
2024-04-27 00:20:55 +0700

    update header

--- View/user/header.php
+++ View/user/header.php
@@ -77,10 +77,78 @@ $total_price_cart=0;
             </li>
         </div>
         <div class="icon-item">
-            <li href="index.php?control=Cart">
+            <li>
                 <ul class="show-item"> 
-                        <a class="a-head" href="index.php?control=Cart"><i class="fa-solid fa-cart-arrow-down" style="color: #e95221;"></i></a>
-                        <span class="icon-name"><a class="a-head" href="index.php?control=Cart"><span class="icon-name" id="spanGH">GIỎ HÀNG</span></a></span>
+                        <i class="fa-solid fa-cart-arrow-down" style="color: #e95221;"></i>
+                        <span class="icon-name"><a class="a-head" href="index.php?control=Cart">Giỏ Hàng</a></span>
+
+                        
+                        <div class="popup-cart">
+                            <div class="title-cart-head">Giỏ hàng</div>
+                                <div class="cart-body">
+                                    <div class="ajaxcart-row">
+                                    <?php 
+                                        require_once('../Model/ModelVariantDetail.php');
+                                        require_once('../Model/ModelUser.php');
+                                        require_once('../Model/ModelProduct.php');
+                                        require_once('../Model/ModelCartDetail.php');
+                                        $modelVariantDetail = new ModelVariantDetail();
+                                        $modelUser = new ModelUser();
+                                        $modelProduct = new ModelProduct();
+                                        $modelCartDetail = new ModelCartDetail();
+                                        $cartDetails=$modelCartDetail->getCartDetailByCartID($modelUser->getUIDByUserName($_SESSION['username']));
+                                        foreach ($cartDetails  as $cartDetail) {
+                                            $variantDetail = $modelVariantDetail->getVariantByID(($cartDetail)->getVariantID());
+                                            $product = $modelProduct->getProductByID(($cartDetail)->getProductID());
+                                            $total_price_cart+=$product->getPrice();
+                                            echo "
+                                            <div class='cart-product'>
+                                        <a href='#' class='cart-image' title='" . $product->getName() . "'><img width='80' height='80' src='../View/images/product/GiayNam.png' 
+                                        alt='" . $product->getName() . "'></a>
+                                        <div class='cart-info'>
+                                            <div class='cart-name'>
+                                                <a href='#' class='' title='" . $product->getName() . "'>" . $product->getName() . "</a>";
+                                                if ($variantDetail->getColor() != null)
+                                                    echo "<span class='variant-title'>Màu: " . $variantDetail->getColor() . "</span>";
+                                                if ($variantDetail->getWeight() != null && $variantDetail->getGrip() != null)
+                                                    echo "<span class='variant-title'>Bản: " . ($variantDetail->getWeight().''.$variantDetail->getGrip()) . "</span>";
+                                                if ($variantDetail->getSize() != null)
+                                                    echo "<span class='variant-title'>Size: " . $variantDetail->getSize() . "</span>";
+                                                if ($variantDetail->getSpeed() != null)
+                                                    echo "<span class='variant-title'>Tốc độ: " . $variantDetail->getSpeed() . "</span>";
+                                                echo "
+                                            </div>
+                                            <div class='grid'>
+                                                <div class='cart-item-name'>
+                                                    <div class='input-group-btn'>
+                                                        <button class='qty-minus' onclick='var result = document.getElementById(\"qtym\"); var qtypro = parseInt(result.value); if (!isNaN(qtypro) && qtypro > 1) result.value = qtypro - 1; return false;' type='button'>-</button>
+                                                        <input type='text' id='qtym' name='so_luong' value='1' maxlength='3' class='in' onkeypress='if (isNaN(this.value + String.fromCharCode(event.keyCode))) return false;' onchange='if(this.value == 0) this.value=1;'>
+                                                        <button class='qty-plus' onclick='var result = document.getElementById(\"qtym\"); var qtypro = parseInt(result.value); if (!isNaN(qtypro)) result.value = qtypro + 1; return false;' type='button'><span>+</span></button>
+                                                    </div>
+                                                </div>
+                                                <div class='cart-prices'>
+                                                    <span class='cart-price'>" . number_format($product->getPrice(), 0, '.', '.') . "₫</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>";
+                                                }
+                                                ?>
+                            
+                                </div>
+                            </div>
+                                <div class="ajaxcart-footer">
+                                    <div class="ajaxcart-subtotal">
+                                        <div class="cart-subtotal">
+                                            <div class="cart-col-6">Tổng tiền:</div>
+                                            <div class="text-right cart-totle"><span class="total-price"><?php echo number_format($total_price_cart , 0, '.', '.'); ?>₫</span></div>
+                                        </div>
+                                    </div>
+                                    <div class="cart-btn-proceed-checkout-dt ">
+                                        <button onclick="location.href='/gio-hang/thanh-toan'" type="button" class="button btn btn-default cart-btn-proceed-checkout" id="btn-proceed-checkout" title="Thanh toán">Đặt hàng</button>
+                                    </div>
+                                </div>
+                        </div>
                 </ul>
             </li>
         </div>
@@ -138,76 +206,9 @@ $total_price_cart=0;
     <li> <a class="titlemenu" href="index.php?control=ContactCategory">LIÊN HỆ</a></li>
 </div>
 
-            <form action class="popup-cart">
-                <div class="title-cart-head">Giỏ hàng</div>
-                <div class="cart-body">
-                    <div class="ajaxcart-row">
-                    <?php 
-                        require_once('../Model/ModelVariantDetail.php');
-                        require_once('../Model/ModelUser.php');
-                        require_once('../Model/ModelProduct.php');
-                        require_once('../Model/ModelCartDetail.php');
-                        $modelVariantDetail = new ModelVariantDetail();
-                        $modelUser = new ModelUser();
-                        $modelProduct = new ModelProduct();
-                        $modelCartDetail = new ModelCartDetail();
-                        $cartDetails=$modelCartDetail->getCartDetailByCartID($modelUser->getUIDByUserName($_SESSION['username']));
-                        foreach ($cartDetails  as $cartDetail) {
-                            $variantDetail = $modelVariantDetail->getVariantByID(($cartDetail)->getVariantID());
-                            $product = $modelProduct->getProductByID(($cartDetail)->getProductID());
-                            $total_price_cart+=$product->getPrice();
-                            echo "
-                            <div class='cart-product'>
-                        <a href='#' class='cart-image' title='" . $product->getName() . "'><img width='80' height='80' src='../View/images/product/GiayNam.png' 
-                        alt='" . $product->getName() . "'></a>
-                        <div class='cart-info'>
-                            <div class='cart-name'>
-                                <a href='#' class='' title='" . $product->getName() . "'>" . $product->getName() . "</a>";
-                            if ($variantDetail->getColor() != null)
-                                echo "<span class='variant-title'>Màu: " . $variantDetail->getColor() . "</span>";
-                            if ($variantDetail->getWeight() != null && $variantDetail->getGrip() != null)
-                                echo "<span class='variant-title'>Bản: " . ($variantDetail->getWeight().''.$variantDetail->getGrip()) . "</span>";
-                            if ($variantDetail->getSize() != null)
-                                echo "<span class='variant-title'>Size: " . $variantDetail->getSize() . "</span>";
-                            if ($variantDetail->getSpeed() != null)
-                                echo "<span class='variant-title'>Tốc độ: " . $variantDetail->getSpeed() . "</span>";
-                            echo "
-                    </div>
-                    <div class='grid'>
-                        <div class='cart-item-name'>
-                            <div class='input-group-btn'>
-                                <button class='qty-minus' onclick='var result = document.getElementById(\"qtym\"); var qtypro = parseInt(result.value); if (!isNaN(qtypro) && qtypro > 1) result.value = qtypro - 1; return false;' type='button'>-</button>
-                                <input type='text' id='qtym' name='so_luong' value='1' maxlength='3' class='in' onkeypress='if (isNaN(this.value + String.fromCharCode(event.keyCode))) return false;' onchange='if(this.value == 0) this.value=1;'>
-                                <button class='qty-plus' onclick='var result = document.getElementById(\"qtym\"); var qtypro = parseInt(result.value); if (!isNaN(qtypro)) result.value = qtypro + 1; return false;' type='button'><span>+</span></button>
-                            </div>
-                        </div>
-                        <div class='cart-prices'>
-                            <span class='cart-price'>" . number_format($product->getPrice(), 0, '.', '.') . "₫</span>
-                        </div>
-                    </div>
-                </div>
-            </div>";
-                        }
-                        ?>
-
-            
-        </div>
-    </div>
-                <div class="ajaxcart-footer">
-                    <div class="ajaxcart-subtotal">
-                        <div class="cart-subtotal">
-                            <div class="cart-col-6">Tổng tiền:</div>
-                            <div class="text-right cart-totle"><span class="total-price"><?php echo number_format($total_price_cart , 0, '.', '.'); ?>₫</span></div>
-                        </div>
-                    </div>
-                    <div class="cart-btn-proceed-checkout-dt ">
-                        <button onclick="location.href='/gio-hang/thanh-toan'" type="button" class="button btn btn-default cart-btn-proceed-checkout" id="btn-proceed-checkout" title="Thanh toán">Đặt hàng</button>
-                    </div>
-                </div>
-            </form>
 <script>
-    const popupCart = document.querySelector('.popup-cart');
-    const showItem = document.querySelector('.show-item');
+    var popupCart = document.querySelector('.popup-cart');
+    var showItem = document.querySelector('.show-item');
 
     // Thêm sự kiện khi di chuột qua biểu tượng giỏ hàng
     showItem.addEventListener('mouseover', function(event) {
@@ -218,10 +219,25 @@ $total_price_cart=0;
     showItem.addEventListener('mouseleave', function(event) {
         const toElement = event.toElement || event.relatedTarget;
         // Kiểm tra xem con trỏ đi ra khỏi phần tử popupCart
-        if (!popupCart.contains(toElement) || showItem.contains(toElement)) {
+        if (!popupCart.contains(toElement) || !showItem.contains(toElement)) {
             popupCart.style.display = "none"; // Ẩn pop-up
         }
     });
 
+    // Thêm sự kiện khi mousedown để theo dõi việc kéo chuột
+    document.addEventListener('mousedown', function(event) {
+        // Kiểm tra xem sự kiện mousedown có xảy ra bên trong popupCart hoặc showItem hay không
+        if (popupCart.contains(event.target) || showItem.contains(event.target)) {
+            isDragging = true; // Đánh dấu là đang kéo chuột
+        } else {
+            isDragging = false; // Đánh dấu là không đang kéo chuột
+            popupCart.style.display = "none"; // Ẩn pop-up
+        }
+    });
+
+    // Thêm sự kiện khi mouseup để cập nhật trạng thái của việc kéo chuột
+    document.addEventListener('mouseup', function(event) {
+        isDragging = false; // Khi nhả chuột, đánh dấu là không đang kéo chuột
+    });
     
 </script>
