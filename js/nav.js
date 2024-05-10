615e2760744628cf3c38d31276d18cb9db3394c3
Dikay3105 <huhuhuhuvbd@gmail.com>
2024-05-10 22:05:03 +0700

    filter product-page

--- js/nav.js
+++ js/nav.js
@@ -1,5 +1,6 @@
 countFilter = 0;
 filterArray = [];
+fullArray = [];
 selectedFilters = {};
 
 window.addEventListener("load", function () {
@@ -21,47 +22,32 @@ window.addEventListener("load", function () {
 function uncheckAllInputs() {
     // Lấy tất cả các phần tử input trên trang
     var inputs = document.querySelectorAll('input[type="checkbox"]');
-    
+
     // Lặp qua mỗi phần tử và bỏ chọn (unchecked) nó
-    inputs.forEach(function(input) {
+    inputs.forEach(function (input) {
         input.checked = false;
     });
 }
 
 function getAllByCatalogAndBrand() {
-    return new Promise(function(resolve, reject) {
         var xhttp = new XMLHttpRequest();
-
         xhttp.onreadystatechange = function () {
             if (this.readyState == 4) {
                 if (this.status == 200) {
                     var listVariantDetails = JSON.parse(this.responseText);
                     filterArray = listVariantDetails;
-                    console.log(filterArray);
-                    resolve(listVariantDetails);
+                    fullArray = listVariantDetails;
                 } else {
                     reject(new Error("Yêu cầu thất bại"));
                 }
             }
         };
-        if(getBrandIdFromUrl()!=null)
-            xhttp.open("GET", "ProductNavController.php?getAll=true&id="+getIdFromUrl()+"&brandID="+getBrandIdFromUrl(), true);
+        if (getBrandIdFromUrl() != null)
+            xhttp.open("GET", "ProductNavController.php?getAll=true&id=" + getIdFromUrl() + "&brandID=" + getBrandIdFromUrl(), true);
         else
-            xhttp.open("GET", "ProductNavController.php?getAll=true&id="+getIdFromUrl(), true);
+            xhttp.open("GET", "ProductNavController.php?getAll=true&id=" + getIdFromUrl(), true);
         xhttp.send();
-    });
 }
-
-// Sử dụng promise
-getAllByCatalogAndBrand()
-    .then(function(data) {
-        filterArray = data;
-    })
-    .catch(function(error) {
-        console.error("Lỗi:", error);
-    });
-
-
 
 function loadPage(page, productsPerPage, id, brandID) {
     var xhttp = new XMLHttpRequest();
@@ -77,13 +63,13 @@ function loadPage(page, productsPerPage, id, brandID) {
                 htmlContent += `<div class="col-6 col-md-3">
                     <div class="item_product_main">
                         <div class="product-thumbnail">
-                            <a class="product_overlay" href="index.php?control=ProductDetail&productID=`+ listVariantDetails[i].productID + `" title="`+ listVariantDetails[i].name + `"></a>
-                            <a class="image_thumb" href="index.php?control=ProductDetail&productID=`+ listVariantDetails[i].productID + `" title="`+ listVariantDetails[i].name + `">
-                                <img width="300" height="300" class="lazyload loaded" src="../View/images/product/`+ listVariantDetails[i].productID + `/`+ match + `/`+ listVariantDetails[i].productID + `.1.png" data-src="https://cdn.shopvnb.com/img/300x300/uploads/gallery/vot-cau-long-victor-brave-sword-ltd-pro-noi-dia-taiwan-jpg-4_1711143954.webp" alt="Vợt Cầu Lông Victor Brave Sword LTD Pro (Nội Địa Taiwan)" data-was-processed="true">
+                            <a class="product_overlay" href="index.php?control=ProductDetail&productID=`+ listVariantDetails[i].productID + `" title="` + listVariantDetails[i].name + `"></a>
+                            <a class="image_thumb" href="index.php?control=ProductDetail&productID=`+ listVariantDetails[i].productID + `" title="` + listVariantDetails[i].name + `">
+                                <img width="300" height="300" class="lazyload loaded" src="../View/images/product/`+ listVariantDetails[i].productID + `/` + match + `/` + listVariantDetails[i].productID + `.1.png" data-src="https://cdn.shopvnb.com/img/300x300/uploads/gallery/vot-cau-long-victor-brave-sword-ltd-pro-noi-dia-taiwan-jpg-4_1711143954.webp" alt="Vợt Cầu Lông Victor Brave Sword LTD Pro (Nội Địa Taiwan)" data-was-processed="true">
                             </a>
                         </div>
                         <div class="product-info">
-                            <h3 class="product-name"><a href="index.php?control=ProductDetail&productID=`+ listVariantDetails[i].productID + `" title="`+ listVariantDetails[i].name + `">` + listVariantDetails[i].name + `</a></h3>
+                            <h3 class="product-name"><a href="index.php?control=ProductDetail&productID=`+ listVariantDetails[i].productID + `" title="` + listVariantDetails[i].name + `">` + listVariantDetails[i].name + `</a></h3>
                             <div class="price-box-nav">
                                 <span class="price">`+ formatPrice(listVariantDetails[i].price) + ` ₫</span>
                             </div>
@@ -106,7 +92,7 @@ function loadPage(page, productsPerPage, id, brandID) {
         }
     };
 
-    if(brandID == null)
+    if (brandID == null)
         xhttp.open("GET", "ProductNavController.php?page=" + parseInt(page) + "&productsPerPage=" + parseInt(productsPerPage) + "&id=" + parseInt(id), true);
     else
         xhttp.open("GET", "ProductNavController.php?page=" + parseInt(page) + "&productsPerPage=" + parseInt(productsPerPage) + "&id=" + parseInt(id) + "&brandID=" + parseInt(brandID), true);
@@ -127,7 +113,7 @@ function loadPageFilter(page, productsPerPage) {
                         <div class="product-thumbnail">
                             <a class="product_overlay" href="index.php?control=ProductDetail&productID=`+ productsOnCurrentPage[i].productID + `" title=""></a>
                             <a class="image_thumb" href="index.php?control=ProductDetail&productID=`+ productsOnCurrentPage[i].productID + `" title="">
-                                <img width="300" height="300" class="lazyload loaded" src="../View/images/product/`+ productsOnCurrentPage[i].productID + `/`+ match + `/`+ productsOnCurrentPage[i].productID + `.1.png" data-src="https://cdn.shopvnb.com/img/300x300/uploads/gallery/vot-cau-long-victor-brave-sword-ltd-pro-noi-dia-taiwan-jpg-4_1711143954.webp" alt="Vợt Cầu Lông Victor Brave Sword LTD Pro (Nội Địa Taiwan)" data-was-processed="true">
+                                <img width="300" height="300" class="lazyload loaded" src="../View/images/product/`+ productsOnCurrentPage[i].productID + `/` + match + `/` + productsOnCurrentPage[i].productID + `.1.png" data-src="https://cdn.shopvnb.com/img/300x300/uploads/gallery/vot-cau-long-victor-brave-sword-ltd-pro-noi-dia-taiwan-jpg-4_1711143954.webp" alt="Vợt Cầu Lông Victor Brave Sword LTD Pro (Nội Địa Taiwan)" data-was-processed="true">
                             </a>
                         </div>
                         <div class="product-info">
@@ -200,8 +186,8 @@ function loadNav(productsPerPage, id, brandID) {
             document.getElementById("number-page").innerHTML = htmlContent;
         }
     };
-    
-    if(brandID==null)
+
+    if (brandID == null)
         xhttp.open("GET", "ProductNavController.php?id=" + id + "&productsPerPage=" + productsPerPage, true);
     else
         xhttp.open("GET", "ProductNavController.php?id=" + id + "&productsPerPage=" + productsPerPage + "&brandID=" + brandID, true);
@@ -302,6 +288,8 @@ function removeFilteredItem(id) {
         filterItem.parentNode.removeChild(filterItem);
         filterItem2.checked = false;
         countFilter -= 1;
+        var event = new Event('change');
+        filterItem2.dispatchEvent(event);
 
         // Cập nhật trạng thái của filter-container
         if (countFilter == 0) {
@@ -325,8 +313,8 @@ function clearAllFiltered() {
     var filterContainerElement = document.getElementById("filter-container");
     filterContainerElement.classList.add("hide");
 
-    loadNav(getProductPerPage(),getIdFromUrl());
-    loadPage(1,getProductPerPage(),getIdFromUrl(), getBrandIdFromUrl());
+    loadNav(getProductPerPage(), getIdFromUrl());
+    loadPage(1, getProductPerPage(), getIdFromUrl(), getBrandIdFromUrl());
 }
 
 function formatTime(timeString) {
@@ -367,36 +355,36 @@ function sapXep(arr, key) {
 }
 
 
-function sortby(key) {console.log(filterArray);
+function sortby(key) {
     if (filterArray.length <= 0) {
         getAllByCatalogAndBrand()
     }
     sapXep(filterArray, key);
-    
+
     loadNavFilter();
-    loadPageFilter(1,getProductPerPage());
+    loadPageFilter(1, getProductPerPage());
 
     document.getElementById("page-config").innerHTML = '<label for="mySelect">Item per page: </label>' +
-                        '<select name="mySelect" id="mySelect" onchange="loadPerPageFilter()">' +
-                        '<option value="4">4</option>' +
-                        '<option value="8" selected>8</option>' +
-                        '<option value="12">12</option>' +
-                        '<option value="16">16</option>' +
-                        '</select>';
-                        if (key == 'alpha-asc') {
-                            document.getElementById('keysort').innerHTML='A → Z';
-                        } else if (key == 'alpha-desc') {
-                            document.getElementById('keysort').innerHTML='Z → A';
-                        } else if (key == 'price-asc') {
-                            document.getElementById('keysort').innerHTML='Giá tăng dần';
-                        } else if (key == 'price-desc') {
-                            document.getElementById('keysort').innerHTML='Giá giảm dần';
-                        } else if (key == 'created-desc') {
-                            document.getElementById('keysort').innerHTML='Hàng mới nhất';
-                        } else if (key == 'created-asc') {
-                            document.getElementById('keysort').innerHTML='Hàng cũ nhất';
-                        }
-    
+        '<select name="mySelect" id="mySelect" onchange="loadPerPageFilter()">' +
+        '<option value="4">4</option>' +
+        '<option value="8" selected>8</option>' +
+        '<option value="12">12</option>' +
+        '<option value="16">16</option>' +
+        '</select>';
+    if (key == 'alpha-asc') {
+        document.getElementById('keysort').innerHTML = 'A → Z';
+    } else if (key == 'alpha-desc') {
+        document.getElementById('keysort').innerHTML = 'Z → A';
+    } else if (key == 'price-asc') {
+        document.getElementById('keysort').innerHTML = 'Giá tăng dần';
+    } else if (key == 'price-desc') {
+        document.getElementById('keysort').innerHTML = 'Giá giảm dần';
+    } else if (key == 'created-desc') {
+        document.getElementById('keysort').innerHTML = 'Hàng mới nhất';
+    } else if (key == 'created-asc') {
+        document.getElementById('keysort').innerHTML = 'Hàng cũ nhất';
+    }
+
 }
 
 
@@ -438,7 +426,7 @@ $(document).ready(function () {
 
 
         if (checkedCount > 0) {
-            if(getBrandIdFromUrl()!=null) {
+            if (getBrandIdFromUrl() != null) {
                 var requestData = {
                     selectedFilters,
                     filter: true,
@@ -454,64 +442,95 @@ $(document).ready(function () {
                     id: getIdFromUrl()
                 };
             }
-            
+
             // Gửi yêu cầu lọc đến máy chủ bằng AJAX
             $.ajax({
                 url: "ProductNavController.php", // Đường dẫn tới file xử lý yêu cầu lọc trên máy chủ
                 type: "GET",
                 data: requestData, // Truyền object chứa thông tin các bộ lọc đã chọn
                 success: function (response) {
+                    var keysort = document.getElementById('keysort').innerText;
                     filterArray = JSON.parse(response);
-                    var totalPages = Math.ceil(filterArray.length / getProductPerPage());
-                    //console.log(filterArray.length);
-                    var htmlContent = '';
+                    if (keysort != "Mặc định") {
+                        if (keysort == "A → Z")
+                            sortby('alpha-asc');
+                        else if (keysort == "Z → A")
+                            sortby('alpha-desc');
+                        else if (keysort == "Giá tăng dần")
+                            sortby('price-asc');
+                        else if (keysort == "Giá giảm dần")
+                            sortby('price-desc');
+                        else if (keysort == "Hàng mới nhất")
+                            sortby('created-asc');
+                        else if (keysort == "Hàng cũ nhất")
+                            sortby('created-desc');
 
-                    for (var i = 1; i <= totalPages; i++) {
-                        if (i == 1) {
-                            htmlContent += '<li onclick="loadPageFilter(' + i + ', ' + getProductPerPage() + ')" class="current-page active" data-page="' + i + '" data-products-per-page="' + getProductPerPage() + '">';
-                            htmlContent += '<a>' + i + '</a>';
-                            htmlContent += '</li>';
-                        } else {
-                            htmlContent += '<li onclick="loadPageFilter(' + i + ', ' + getProductPerPage() + ')" data-page="' + i + '" data-products-per-page="' + getProductPerPage() + '">';
-                            htmlContent += '<a>' + i + '</a>';
-                            htmlContent += '</li>';
+                    } else {
+                        var totalPages = Math.ceil(filterArray.length / getProductPerPage());
+                        var htmlContent = '';
+
+                        for (var i = 1; i <= totalPages; i++) {
+                            if (i == 1) {
+                                htmlContent += '<li onclick="loadPageFilter(' + i + ', ' + getProductPerPage() + ')" class="current-page active" data-page="' + i + '" data-products-per-page="' + getProductPerPage() + '">';
+                                htmlContent += '<a>' + i + '</a>';
+                                htmlContent += '</li>';
+                            } else {
+                                htmlContent += '<li onclick="loadPageFilter(' + i + ', ' + getProductPerPage() + ')" data-page="' + i + '" data-products-per-page="' + getProductPerPage() + '">';
+                                htmlContent += '<a>' + i + '</a>';
+                                htmlContent += '</li>';
+                            }
                         }
+
+                        document.getElementById("number-page").innerHTML = htmlContent;
+
+                        //--------------------------//
+                        loadPageFilter(1, getProductPerPage());
+
+                        //--------------------------//
+                        document.getElementById("page-config").innerHTML = '<label for="mySelect">Item per page: </label>' +
+                            '<select name="mySelect" id="mySelect" onchange="loadPerPageFilter()">' +
+                            '<option value="4">4</option>' +
+                            '<option value="8" selected>8</option>' +
+                            '<option value="12">12</option>' +
+                            '<option value="16">16</option>' +
+                            '</select>';
                     }
-
-                    document.getElementById("number-page").innerHTML = htmlContent;
-
-                    //--------------------------//
-                    loadPageFilter(1, getProductPerPage());
-
-                    //--------------------------//
-                    document.getElementById("page-config").innerHTML = '<label for="mySelect">Item per page: </label>' +
-                        '<select name="mySelect" id="mySelect" onchange="loadPerPageFilter()">' +
-                        '<option value="4">4</option>' +
-                        '<option value="8" selected>8</option>' +
-                        '<option value="12">12</option>' +
-                        '<option value="16">16</option>' +
-                        '</select>';
-
                 }
             });
         } else {
-            // Lấy id từ đường dẫn URL
-            var id = getIdFromUrl();
-            // Gọi loadPage và loadNav với id và số 6
-            loadNav(8, id, getBrandIdFromUrl());
-            loadPage(1, 8, id, getBrandIdFromUrl());
-            getAllByCatalogAndBrand();
+            var keysort = document.getElementById('keysort').innerText;
+            filterArray = fullArray;
+            if (keysort != "Mặc định") {
+                if (keysort == "A → Z")
+                    sortby('alpha-asc');
+                else if (keysort == "Z → A")
+                    sortby('alpha-desc');
+                else if (keysort == "Giá tăng dần")
+                    sortby('price-asc');
+                else if (keysort == "Giá giảm dần")
+                    sortby('price-desc');
+                else if (keysort == "Hàng mới nhất")
+                    sortby('created-asc');
+                else if (keysort == "Hàng cũ nhất")
+                    sortby('created-desc');
+            } else {
+                // Lấy id từ đường dẫn URL
+                var id = getIdFromUrl();
+                // Gọi loadPage và loadNav với id và số 6
+                loadNav(8, id, getBrandIdFromUrl());
+                loadPage(1, 8, id, getBrandIdFromUrl());
+                document.getElementById("page-config").innerHTML = '<label for="mySelect">Item per page: </label>' +
+                    '<select name="mySelect" id="mySelect" onchange="loadPerPage()">' +
+                    '<option value="4">4</option>' +
+                    '<option value="8" selected>8</option>' +
+                    '<option value="12">12</option>' +
+                    '<option value="16">16</option>' +
+                    '</select>';
+            }
+
             if (countFilter == 0) {
                 document.getElementById("filter-container").classList.add("hide");
             }
-
-            document.getElementById("page-config").innerHTML = '<label for="mySelect">Item per page: </label>' +
-                '<select name="mySelect" id="mySelect" onchange="loadPerPage()">' +
-                '<option value="4">4</option>' +
-                '<option value="8" selected>8</option>' +
-                '<option value="12">12</option>' +
-                '<option value="16">16</option>' +
-                '</select>';
         }
     });
 });
